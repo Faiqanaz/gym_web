bc6c1df4c2adfef9c3ee74bef7af3e2d73a5b7e3
Faiqanaz <6901213@gmail.com>
2024-12-28 19:14:32 +0500

    adding files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask , render_template # type: ignore
+
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("aboutus.html")
+
+@app.route("/sigin")
+def sigin():
+    return render_template("sigin.html")
+
+if __name__== "__main__":
+    app.run(debug=True)
+
+
+
+    
